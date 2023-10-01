7c2811014ff74d212901d7f10d3732583a4f6997
Gabriel Noronha <inc.691@gmail.com>
2023-10-01 16:43:07 -0300

    Initial setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { app } from "./app";
+
+const PORT = Number(process.env.PORT) || 3333
+
+app.listen({
+  port: PORT,
+  host: '0.0.0.0'
+}).then(() => {
+  console.log('🚀HTTP Server running!')
+})
