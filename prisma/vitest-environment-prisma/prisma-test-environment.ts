591ab374a6c727a57742ff29295ebca70010bd5f
Gabriel Noronha <inc.691@gmail.com>
2023-10-05 15:39:31 -0300

    Implement unique database to each e2e test suit

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,43 @@
-// eslint-disable-next-line import/named
+/* eslint-disable import/named */
+import 'dotenv/config'
+
+import { randomUUID } from 'crypto'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
 import { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
+
+const generateDatabaseURL = (schema: string) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'web',
   async setup() {
-    console.log('setup')
+    const schema = randomUUID()
+    const URL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = URL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
